1234158a02553517db967a2a6574ac8476917489
Cryptoskeptic <hammadr097@gmail.com>
2018-01-09 06:11:44 +0500

    updated signup

--- pages/signup-page.php
+++ pages/signup-page.php
@@ -21,24 +21,25 @@ else {
     if (isset($_POST['submit'])) { 
 
     $fullname = inputFilter($_POST['fullname']);
-    $username = inputFilter($_POST['username']);
     $gender = inputFilter($_POST['optionsRadios']);
     $dob = inputFilter($_POST['dob']);
     $email = $_POST['email'];
     $password = inputFilter($_POST['password']);
 
-
+    $exploded=explode(" ",$fullname);
     // echo "<script>alert('$fullname')</script>";
     // echo "<script>alert('$username')</script>";
     // echo "<script>alert('$gender')</script>";
     // echo "<script>alert('$dob')</script>";
     // echo "<script>alert('$email')</script>";
     // echo "<script>alert('$password')</script>";
-
     supInserter($email,$password,$dob,$gender);       //insert values into database table user 
-    $userID = idAccess($username);                       //get userID    
-                             //create new entry in userProfile table corresponding to userID
-    $profileID = pidAccess($userID);                     //get profile ID of user
+    //$userID = idAccess($username);                       //get userID    
+     //create new entry in userProfile table corresponding to userID
+
+    //profileInserter($userID,$exploded[0], $exploded[1],$Location, $about);
+
+    //$profileID = pidAccess($userID);                     //get profile ID of user
     $_SESSION['uid'] = $userID;                          //initialize new session variable 
     header("location: profile-page.php");                // Redirecting To User Profile Page 
 
